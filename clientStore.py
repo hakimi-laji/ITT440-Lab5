a5d6c9665ade04386a83dc88709542da2234afaf
Hakimi Laji <72840941+hakimi-laji@users.noreply.github.com>
2020-11-27 14:56:06 +0800

    using socket to store client files into the server

---
+++ clientStore.py
@@ -0,0 +1,43 @@
+# Written and modifed by Muhammad Hakimi bin Mohd Laji (2019290708)             #
+# Based on codes from https://linuxhint.com/python_socket_file_transfer_send/   #
+# Simple Python code for file transfer from client to server                    #
+# This code is for CLIENT side                                                  #
+
+# Importing libraries
+import socket
+import sys
+
+# Lets catch the 1st argument as server ip
+if (len(sys.argv) > 1):
+    ServerIp = sys.argv[1]
+else:
+    print("\nRun like \n python3 clientStore.py < server ip address > \n\n")
+    exit(1)
+
+
+# Now we can create socket object
+s = socket.socket()
+
+# Lets choose one port and connect to that port
+PORT = 9898
+
+# Lets connect to that port where server may be running
+s.connect((ServerIp, PORT))
+
+# We can send any file by typing its name including the extension (example: boi.c)
+filetosend = input('\nEnter the file name to be stored in server: ')     
+s.send(filetosend.encode())                     
+file = open(filetosend, "rb")                   
+
+SendData = file.read(99999)
+
+while SendData:
+    # Now we can receive data from server
+    print("\n>Message received from the server:", s.recv(1024).decode("utf-8"))
+    #Now send the content of the file to server
+    s.send(SendData)
+    SendData = file.read(99999)
+    print(">" + filetosend + " has been copied successfully to the server for storage.\n")
+    
+# Close the connection from client side
+s.close()
